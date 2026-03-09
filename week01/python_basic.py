4bc816505f99432d6c535f24511d996e057b4ec9
문태원 <gg93595467@gmail.com>
2026-03-09 15:49:07 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -382,3 +382,12 @@ print(c)
 # 사용자 입력으로 받은 값을 문자열로 반환2
 c = input("정수를 입력하세요: ")
 print(c)
+
+# 입력 받은 숫자에 해당되는 범위의 값을 반환
+print(range(5))
+print(list(range(5)))
+print(list(range(5, 10)))
+print(list(range(5, 10, 2)))
+
+for i in range(5, 20, 3):
+    print(i)
