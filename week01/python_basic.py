8f44d9738db4ee00aa35b5f4dac23b594b96d8f8
문태원 <gg93595467@gmail.com>
2026-03-10 09:41:04 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -202,8 +202,8 @@ for key in dic.keys():
     print(dic[key])
 
 # value의 리스트 만들기
-print(dic.value())
-print(list(dic.value()))
+print(dic.values())
+print(list(dic.values()))
 
 # key, value 쌍 구하기
 print(dic.items())
