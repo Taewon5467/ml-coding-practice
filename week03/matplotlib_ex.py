f71b397e4a11a33039d8ba5aefe245ed60fc255c
문태원 <gg93595467@gmail.com>
2026-03-23 10:42:17 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -316,3 +316,18 @@ plt.close()
 
 ## **개별 서브플롯을 동시에 생성하기**
 
+# 2*2 구성의 figure
+fig, axes = plt.subplots(2, 2)
+
+# 선 그래프
+axes[0, 0].plot([1, 2, 3])
+axes[0, 1].plot([4, 5, 6])
+axes[1, 0].plot([7, 8, 9])
+axes[1, 1].plot([10, 11, 12])
+plt.savefig('Figure14.png')
+plt.close()
+
+## **타이타닉 데이터셋으로 개별 서브플롯 동시에 그리기**
+
+# 각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
+parch_counts = titanic.groupby('Parch')['Survived'].value_counts
