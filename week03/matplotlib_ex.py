fff6e7a190604199952b54ba7b7d3c1af9585a72
문태원 <gg93595467@gmail.com>
2026-03-23 10:36:46 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -301,3 +301,5 @@ plt.tick_params(axis='y', labelcolor='indigo')
 plt.legend(loc='upper right')
 
 # 두 번째 서브플롯 설정(막대 그래프)
+plt.subplot(2, 1, 2)
+plt.bar(x, y2, color='deeppink', alpha=0.7, width=0.5, label='Survived')
