a6dced51c6f0c973cfbf17df3bc8378846fe63a5
문태원 <gg93595467@gmail.com>
2026-03-30 10:07:44 +0900

    Updated Changes

--- week03/data_preprocessing.py
+++ week03/data_preprocessing.py
@@ -31,3 +31,7 @@ start_train_set, start_test_set = train_test_split(
 
 for set_ in (start_train_set, start_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
+
+"""
+* 원본 훈련 세트로 복원하고 타깃을 분리
+* 'strat_train_set.drop()'은 지정한 열을 제외한 'strat_train_set'의 """
