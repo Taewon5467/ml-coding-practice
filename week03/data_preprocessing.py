12eeb4b382aabcb83a6b7c1c7702c0788c51d887
문태원 <gg93595467@gmail.com>
2026-03-30 10:08:14 +0900

    Updated Changes

--- week03/data_preprocessing.py
+++ week03/data_preprocessing.py
@@ -34,4 +34,5 @@ for set_ in (start_train_set, start_test_set):
 
 """
 * 원본 훈련 세트로 복원하고 타깃을 분리
-* 'strat_train_set.drop()'은 지정한 열을 제외한 'strat_train_set'의 """
+* 'strat_train_set.drop()'은 지정한 열을 제외한 'strat_train_set'의 복사본을 만듦
+* 'inplace=True'로 지정하지 않은 한 'st"""
