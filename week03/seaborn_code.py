e3d9e7b9a126ece6e0d0e597923fedb95490bd2b
문태원 <gg93595467@gmail.com>
2026-03-30 09:50:44 +0900

    Updated Changes

--- week03/seaborn_code.py
+++ week03/seaborn_code.py
@@ -74,4 +74,9 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 # **조인트 그래프**
 # jointplot( ) 그리기
 sns.jointplot(x='size', y='tip', data=tips, kind='scatter')
-plt.savefig()
+plt.savefig('./week04/Seaborn_Figure05.jpg')
+
+# **관계 그래프**
+# pairplot( ) 그리기
+sns.jointplot(x='size', y='tip', data=tips, kind='scatter')
+plt.
