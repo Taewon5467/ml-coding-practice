0cd4ebd3b20fd96da019bcc31d10dfc0e3faaba7
문태원 <gg93595467@gmail.com>
2026-05-11 09:30:03 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -28,4 +28,6 @@ from sklearn.linear_model import LinearRegression
 
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
-lin.reg.intercept_, lin_reg.coef_
+lin_reg.intercept_, lin_reg.coef_
+
+X_new = np.array([0], [2])
