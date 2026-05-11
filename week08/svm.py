6d579402c66c045a54452cfceea0d3d6f9512e01
문태원 <gg93595467@gmail.com>
2026-05-11 10:11:58 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -2,6 +2,7 @@
 # 선형 SVM 분류
 
 import numpy as np
+from sklearn.datasets import load_iris
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from 
+from sklearn.svm import LinearSVC
